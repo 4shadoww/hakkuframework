52f1e4706afc982eb7e618157b87ecd86883fd24
4shadoww <4shadoww0@gmail.com>
2016-06-09 13:13:38 +0300

    module hostname_resolver added

---
+++ modules/hostname_resolver.py
@@ -0,0 +1,43 @@
+#        Copyright (C) 2015 Noa-Emil Nissinen (4shadoww)
+import sys
+from core import bcolors
+from collections import OrderedDict
+import socket
+
+
+#info about module
+#modules name (must be same as filename)
+modulename = "hostname_resolver"
+#module version
+version = "1.0"
+#description
+desc = "resolver hostname using ip"
+#creator's github
+github = "4shadoww"
+#created by (creators name)
+createdby = "4shadoww"
+#email
+email = "4shadoww0@gmail.com"
+
+#list of variables
+variables = OrderedDict((
+('target', '192.168.1.1'),
+))
+
+#description for variables
+vdesc = [
+'target ip address',
+]
+
+
+#simple changelog
+changelog = bcolors.YEL+"Version 1.0:\nrelease"+bcolors.END
+
+def run():
+	try:
+		querly = socket.gethostbyaddr(variables['target'])
+		print(bcolors.YEL+"resolved hostname:", querly[0]+bcolors.END)
+	except(socket.herror):
+		print(bcolors.WARNING+"unknown host"+bcolors.END)
+	except(socket.gaierror):
+		print(bcolors.WARNING+"name or service not known"+bcolors.END)
