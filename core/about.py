04d0b5fa57c63b2ce0750eef227e4e8cbd083122
4shadoww <4shadoww0@gmail.com>
2016-06-11 18:07:27 +0300

    added colors

--- core/about.py
+++ core/about.py
@@ -2,8 +2,8 @@ from core import bcolors
 from core.info import version
 
 def print_about():
-    print("version: "+ '\xb5' +"Sploit Framework " + version)
-    print("created by: Noa-Emil Nissinen (4shadoww)")
-    print("email: 4shadoww0@gmail.com")
-    print("github: 4shadoww")
-    print("twitter: dev4shadoww")
+    print(bcolors.OKBLUE+"version: "+bcolors.END+ '\xb5' +"Sploit Framework " + version)
+    print(bcolors.OKBLUE+"created by:"+bcolors.END+" Noa-Emil Nissinen (4shadoww)")
+    print(bcolors.OKBLUE+"email:"+bcolors.END+" 4shadoww0@gmail.com")
+    print(bcolors.OKBLUE+"github:"+bcolors.END+" 4shadoww")
+    print(bcolors.OKBLUE+"twitter:"+bcolors.END+" dev4shadoww")
