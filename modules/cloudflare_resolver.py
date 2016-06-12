491de8029d74144fb16e5432f04e39e7b0783e93
4shadoww <4shadoww0@gmail.com>
2016-06-12 17:01:40 +0300

    updated cloudflare_resolver module

--- modules/cloudflare_resolver.py
+++ modules/cloudflare_resolver.py
@@ -29,22 +29,24 @@ variables = OrderedDict((
 ('target', 'google.com'),
 ('alert', 'false'),
 ('pos', 'false'),
+('timeout', '0.5')
 ))
 
 vdesc = [
 'target address',
 'alert when done(beep)[true/false]',
 'print only success[true/false]',
+'timeout',
 ]
 
 #simple changelog
-changelog = "Version 1.0:\nrelease\n\nVersion 2.0:\n+ fixed timeout bug\n+ module is now using dns library\n\nVersion 2.1:\n+ added more colors"
+changelog = "Version 1.0:\nrelease\n\nVersion 2.0:\n+ fixed timeout bug\n+ module is now using dns library\n\nVersion 2.1:\n+ added more colors\n\nVersion 2.2:\n+ added ? when resolved is same than default ip\n+ added timeout variable"
 
 #run function
 def run():
 	ipresolver = resolver.Resolver()
-	ipresolver.timeout = 1
-	ipresolver.lifetime = 1
+	ipresolver.timeout = float(variables['timeout'])
+	ipresolver.lifetime = float(variables['timeout'])
 
 	variables['target'] = variables['target'].replace("http://", "")
 	variables['target'] = variables['target'].replace("https://", "")
@@ -65,7 +67,7 @@ def run():
 		try:
 			query = ipresolver.query(host, 'A')
 			if query[0] == orgip[0]:
-				print(bcolors.YEL+"[+] %s : %s"%(host, query[0])+bcolors.END)
+				print(bcolors.YEL+"[?] %s : %s"%(host, query[0])+bcolors.END)
 			else:
 				print(bcolors.OKGREEN+"[+] %s : %s"%(host, query[0])+bcolors.END)
 		except(dns.exception.Timeout):
