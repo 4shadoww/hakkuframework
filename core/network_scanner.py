770bfd95f7153e9d7729a8516c1a2187fc9c4588
4shadoww <4shadoww0@gmail.com>
2016-06-13 15:52:45 +0300

    updated network_scanner

--- core/network_scanner.py
+++ core/network_scanner.py
@@ -7,34 +7,23 @@ from core import bcolors
 
 def scan():
 	print(bcolors.OKBLUE+"interfaces:"+bcolors.END)
-	try:
-		for iface in netifaces.interfaces():
-			print(bcolors.YEL+iface+bcolors.END)
-	except(ValueError):
-		print(bcolors.WARNING+"error: invalid range"+bcolors.END)
-		return
+	for iface in netifaces.interfaces():
+		print(bcolors.YEL+iface+bcolors.END)
 	print("")
 	interface = input(bcolors.HEADER+"interface: "+bcolors.END)
-	print(bcolors.OKBLUE+"\n[*] default range 24"+bcolors.END)
-	iprange = input(bcolors.HEADER+"range: "+bcolors.END)
 	try:
 		ip = netifaces.ifaddresses(interface)[2][0]['addr']
 	except(ValueError):
 		print(bcolors.WARNING+"error: invalid interface"+bcolors.END)
 		return
-	ips = ip+"/"+iprange
+	ips = ip+"/24"
 	print(bcolors.OKGREEN+"\n[*] scanning...\n"+bcolors.END)
+	print(bcolors.OKBLUE+"MAC - IP"+bcolors.END)
 
 	start_time = datetime.now()
 
 	conf.verb = 0
-	try:
-		ans, unans = srp(Ether(dst="ff:ff:ff:ff:ff:ff")/ARP(pdst = ips), timeout = 2,iface=interface,inter=0.1)
-	except(ValueError):
-		print(bcolors.WARNING+"error: invalid range"+bcolors.END)
-		return
-
-	print(bcolors.OKBLUE+"MAC - IP"+bcolors.END)
+	ans, unans = srp(Ether(dst="ff:ff:ff:ff:ff:ff")/ARP(pdst = ips), timeout = 2,iface=interface,inter=0.1)
 
 	for snd,rcv in ans:
 		print(rcv.sprintf(bcolors.YEL+"r%Ether.src% - %ARP.psrc%"+bcolors.END))
