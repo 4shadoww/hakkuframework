7ffe4845305b20acbe23dd8ef17d1f9825e8d265
4shadoww <4shadoww0@gmail.com>
2016-06-17 17:33:59 +0300

    added new module webserver_scout

---
+++ modules/webserver_scout.py
@@ -0,0 +1,51 @@
+#        Copyright (C) 2015 Noa-Emil Nissinen (4shadoww)
+import sys
+from core import bcolors
+from collections import OrderedDict
+import http.client
+from core.errormessage import printerror
+import socket
+
+#info about module
+#modules name (must be same as filename)
+modulename = "webserver_scout"
+#module version
+version = "1.0"
+#description
+desc = "get information from webserver"
+#creator's github
+github = "4shadoww"
+#created by (creators name)
+createdby = "4shadoww"
+#email
+email = "4shadoww0@gmail.com"
+#alert user if root permissions not available (remove variable below if root permissions needed)
+
+#list of variables
+variables = OrderedDict((
+('target', 'google.com'),
+))
+
+#description for variables
+vdesc = [
+'description1',
+]
+
+
+#simple changelog
+changelog = "Version 1.0:\nrelease"
+
+def run():
+	try:
+		conn = http.client.HTTPConnection(variables['target'])
+		conn.request("HEAD","/index.html")
+		res = conn.getresponse()
+		results = res.getheaders()
+		print('')
+		for item in results:
+			print(bcolors.YEL+item[0], item[1]+bcolors.END)
+		print('')
+	except http.client.InvalidURL:
+		printerror('invalid url')
+	except socket.gaierror:
+		printerror('name or service not known')
