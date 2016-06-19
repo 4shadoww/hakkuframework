8a32eaf723204248b9156b2f1217c181d392a7f5
4shadoww <4shadoww0@gmail.com>
2016-06-19 19:49:08 +0300

    updated

--- core/messages.py
+++ core/messages.py
@@ -1,7 +1,15 @@
 from core import bcolors
 
 def printerror(message):
-    print(bcolors.WARNING+'error:',message+bcolors.END)
+    if "\n" in message[0:1]:
+            message = message.replace(message[0:1], "")
+            print(bcolors.WARNING+'\nerror:',message+bcolors.END)
+    else:
+        print(bcolors.WARNING+'error:',message+bcolors.END)
 
 def printsuccess(message):
-    print(bcolors.OKGREEN+'[*]', message+bcolors.END)
+    if "\n" in message[0:1]:
+        message = message.replace(message[0:1], "")
+        print(bcolors.OKGREEN+'\n[*]', message+bcolors.END)
+    else:
+         print(bcolors.OKGREEN+'[*]', message+bcolors.END)
