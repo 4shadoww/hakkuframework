911178f17921a2c4fbbf70609891b3f9aceed32e
4shadoww <4shadoww0@gmail.com>
2016-11-29 19:54:30 +0200

    tiny changes

--- core/info.py
+++ core/info.py
@@ -1,3 +1,11 @@
-version = "1.3-alpha"
-update_date = "20.6.2016"
-codename = "New Beginning"
+from core import colors
+
+version = "2.0-alpha"
+update_date = "22.11.2016"
+codename = "Dark Side"
+
+about = (colors.blue+"version: "+colors.end+ '\xb5' +"Sploit Framework " + version
++colors.blue+"\ncreated by:"+colors.end+" Noa-Emil Nissinen (4shadoww)"
++colors.blue+"\nemail:"+colors.end+" 4shadoww0@gmail.com"
++colors.blue+"\ngithub:"+colors.end+" 4shadoww"
++colors.blue+"\ntwitter:"+colors.end+" dev4shadoww")
