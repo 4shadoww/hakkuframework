9fb75d519c500e42ce74a5b249922d84f91072e3
4shadoww <4shadoww0@gmail.com>
2016-11-29 19:51:04 +0200

    removed

--- core/about.py
+++
@@ -1,9 +0,0 @@
-from core import bcolors
-from core.info import version
-
-def print_about():
-    print(bcolors.OKBLUE+"version: "+bcolors.END+ '\xb5' +"Sploit Framework " + version)
-    print(bcolors.OKBLUE+"created by:"+bcolors.END+" Noa-Emil Nissinen (4shadoww)")
-    print(bcolors.OKBLUE+"email:"+bcolors.END+" 4shadoww0@gmail.com")
-    print(bcolors.OKBLUE+"github:"+bcolors.END+" 4shadoww")
-    print(bcolors.OKBLUE+"twitter:"+bcolors.END+" dev4shadoww")
