15b1bff932a433e690b9d618df5999588500ee74
4shadoww <4shadoww0@gmail.com>
2016-11-29 20:00:41 +0200

    tiny changes

--- core/setvar.py
+++ core/setvar.py
@@ -1,7 +1,7 @@
 #        Copyright (C) 2015 Noa-Emil Nissinen (4shadoww)
 import __main__
-from core import bcolors
+from core import colors
 
 def setvar(variable, value):
 	__main__.funcname.variables[variable] = value
-	print(bcolors.OKGREEN+variable +" => "+ value + bcolors.END)
+	print(colors.green+variable +" => "+ value + colors.end)
