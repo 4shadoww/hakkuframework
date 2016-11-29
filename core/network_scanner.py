92249f97ba40f0911b93d56352cbf975194902b2
4shadoww <4shadoww0@gmail.com>
2016-11-29 19:59:35 +0200

    tiny changes

--- core/network_scanner.py
+++ core/network_scanner.py
@@ -3,29 +3,30 @@ import os
 from scapy.all import srp,Ether,ARP,conf
 from datetime import datetime
 import traceback
-from core import bcolors
+from core import colors
+
 try:
 	import netifaces
 except ImportError:
-	print(bcolors.WARNING+'netifaces import error:\n')
+	print(colors.red+'netifaces import error:\n')
 	traceback.print_exc()
-	print(bcolors.END)
+	print(colors.end)
 
 
 def scan():
-	print(bcolors.OKBLUE+"interfaces:"+bcolors.END)
+	print(colors.blue+"interfaces:"+colors.end)
 	for iface in netifaces.interfaces():
-		print(bcolors.YEL+iface+bcolors.END)
+		print(colors.yellow+iface+colors.end)
 	print("")
-	interface = input(bcolors.HEADER+"interface: "+bcolors.END)
+	interface = input(colors.purple+"interface: "+colors.end)
 	try:
 		ip = netifaces.ifaddresses(interface)[2][0]['addr']
 	except(ValueError):
-		print(bcolors.WARNING+"error: invalid interface"+bcolors.END)
+		print(colors.red+"error: invalid interface"+colors.end)
 		return
 	ips = ip+"/24"
-	print(bcolors.OKGREEN+"\n[*] scanning please wait...\n"+bcolors.END)
-	print(bcolors.OKBLUE+"MAC - IP"+bcolors.END)
+	print(colors.green+"\n[*] scanning please wait...\n"+colors.end)
+	print(colors.blue+"MAC - IP"+colors.end)
 
 	start_time = datetime.now()
 
@@ -33,12 +34,12 @@ def scan():
 	try:
 		ans, unans = srp(Ether(dst="ff:ff:ff:ff:ff:ff")/ARP(pdst = ips), timeout = 2,iface=interface,inter=0.1)
 	except PermissionError:
-		print(bcolors.WARNING+'error: root permissions required')
+		print(colors.red+'error: root permissions required')
 		return
 
 	for snd,rcv in ans:
-		print(rcv.sprintf(bcolors.YEL+"r%Ether.src% - %ARP.psrc%"+bcolors.END))
+		print(rcv.sprintf(colors.yellow+"r%Ether.src% - %ARP.psrc%"+colors.end))
 	stop_time = datetime.now()
 	total_time = stop_time - start_time
-	print(bcolors.OKGREEN+"\n[*] scan completed")
-	print("[*] scan duration:",total_time,bcolors.END)
+	print(colors.green+"\n[*] scan completed")
+	print("[*] scan duration:",total_time,colors.end)
