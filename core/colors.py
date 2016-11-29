10f3e347620140f71f0de10a9ec30930685cbdee
4shadoww <4shadoww0@gmail.com>
2016-11-29 19:52:28 +0200

    bcolors renamed to colors

---
+++ core/colors.py
@@ -0,0 +1,11 @@
+#        Copyright (C) 2015 Noa-Emil Nissinen (4shadoww)
+
+purple = '\033[95m'
+blue = '\033[94m'
+green = '\033[32m'
+cyan = '\033[36m'
+yellow = '\033[93m'
+red = '\033[91m'
+end = '\033[0m'
+bold = '\033[1m'
+uline = '\033[4m'
