98eb6847809e0fc18c4b80175c46522524c5830d
4shadoww <4shadoww0@gmail.com>
2016-12-11 12:36:56 +0200

    added class

--- core/cmethods.py
+++ core/cmethods.py
@@ -29,289 +29,284 @@ import core.touchingsky
 # Import modules
 from modules import *
 
-# Module manager object
+class Cmethods:
 
-global mm
+	# Module manager object
 
-# Init
+	mm = None
+	modadd = None
 
-def init(mmi):
-	global mm
-	mm = mmi
+	# Init
 
-# Module custom commands
+	def __init__(self, mmi):
+		self.mm = mmi
 
-def mcu(command):
-	global modadd
+	# Module custom commands
 
-	try:
-		if command in modadd.customcommands:
+	def mcu(self, command):
+
+		try:
+			if command in self.modadd.customcommands:
+				try:
+					self.modadd.terminal = terminal
+				except NameError:
+					pass
+				call = getattr(self.modadd, command)
+				try:
+					call()
+				except Exception as e:
+					print(colors.red+"error: module is corrupted\n")
+					traceback.print_exc(file=sys.stdout)
+					print(colors.end)
+			else:
+				raise UnknownCommand("unknown command")
+		except AttributeError:
+			raise UnknownCommand("unknown command")
+
+	# Built in commands
+
+	def exit(self, args):
+		if self.mm.moduleLoaded == 1:
+			self.mm.moduleLoaded = 0
+			self.mm.moduleName = ""
+		else:
+			sys.exit()
+
+	def clear(self, args):
+		sys.stderr.write("\x1b[2J\x1b[H")
+
+	def use(self, args):
+		self.mm.loadModule()
+		self.mm.setName(args[0])
+
+	def cl(self, args):
+		os.system(' '.join(args))
+
+	def help(self, args):
+
+		if self.mm.moduleLoaded == 0:
+			print(helptable.generateTable(helpin.commands))
+		else:
+			try: 
+				print(helptable.generatemTable(helpin.mcommands, self.modadd.mhelp))
+			except AttributeError:
+				print(helptable.generateTable(helpin.mcommands))
 			try:
-				modadd.terminal = terminal
-			except NameError:
+				print('\n',self.modadd.help_notes,'\n')
+			except(AttributeError):
 				pass
-			call = getattr(modadd, command)
+
+	def version(self, args):
+
+		if self.mm.moduleLoaded == 1:
+			print(self.modadd.name+" "+self.modadd.version)
+		else:
+			print("µSploit Framework " + info.version)
+
+	def ifconfig(self, args):
+		os.system("ifconfig"+" "+' '.join(args))
+
+	def scan(self, args):
+		network_scanner.scan()
+
+	def about(self, args):
+		print(info.about)
+
+	def changelog(self, args):
+		if self.mm.moduleLoaded == 1:
 			try:
-				call()
-			except Exception as e:
+				print(colors.yellow+self.modadd.changelog+colors.end)
+			except Exception as error:
 				print(colors.red+"error: module is corrupted\n")
 				traceback.print_exc(file=sys.stdout)
 				print(colors.end)
 		else:
-			raise UnknownCommand("unknown command")
-	except AttributeError:
-		raise UnknownCommand("unknown command")
-
-# Built in commands
-
-def exit(args):
-	if mm.moduleLoaded == 1:
-		mm.moduleLoaded = 0
-		mm.moduleName = ""
-	else:
-		sys.exit()
-
-def clear(args):
-	sys.stderr.write("\x1b[2J\x1b[H")
-def use(args):
-	global mm
-	mm.loadModule()
-	mm.setName(args[0])
-
-def cl(args):
-	os.system(' '.join(args))
-
-def help(args):
-	global modadd
-
-	if mm.moduleLoaded == 0:
-		print(helptable.generateTable(helpin.commands))
-	else:
-		try: 
-			print(helptable.generatemTable(helpin.mcommands, modadd.mhelp))
-		except AttributeError:
-			print(helptable.generateTable(helpin.mcommands))
-		try:
-			print('\n',modadd.help_notes,'\n')
-		except(AttributeError):
-			pass
-
-def version(args):
-	global modadd
-
-	if mm.moduleLoaded == 1:
-		print(modadd.name+" "+modadd.version)
-	else:
-		print("µSploit Framework " + info.version)
-
-def ifconfig(args):
-	os.system("ifconfig"+" "+' '.join(args))
-
-def scan(args):
-	network_scanner.scan()
-
-def about(args):
-	print(info.about)
-
-def changelog(args):
-	global modadd
-	if mm.moduleLoaded == 1:
-		try:
-			print(colors.yellow+modadd.changelog+colors.end)
-		except Exception as error:
-			print(colors.red+"error: module is corrupted\n")
-			traceback.print_exc(file=sys.stdout)
-			print(colors.end)
-	else:
-		try:
-			f = open('changelog', 'r')
-			file_contents = f.read()
-			print (colors.yellow+file_contents+colors.end)
-			f.close()
-		except(IOError):
-			print(colors.red + "error: changelog file not found (have you removed it?)" + colors.end)
-
-def use(args):
-	global modadd
-	if mm.moduleLoaded == 0:
-		try:
-			modadd = globals()[args[0]]
-			mm.moduleLoaded = 1
-			mm.setName(modadd.name)
 			try:
-				if modadd.outdated == 1:
-					print(colors.red+"this module is outdated and might not be working"+colors.end)
-			except (AttributeError):
-				pass
-			try:
-				if modadd.needroot == 1:
-					if not os.geteuid() == 0:
-						print(colors.red+"this module requires root permissions for full functionality!"+colors.end)
-			except(AttributeError):
-				pass
-			try:
-				modadd.init()
-			except(AttributeError):
-				pass
-		except KeyError:
-			print(colors.red + "module not found" + colors.end)
-		except IndexError:
-			print(colors.red + "please enter module name" + colors.end)
-	else:
-		raise UnknownCommand("module in use")
-
-def show(args):
-	try:
-		if args[0] == "modules" and mm.moduleLoaded == 0:
-			t = PrettyTable([colors.green+'Modules:', ''+colors.end])
-			t.add_row(['',''])
-			t.align = 'l'
-			t.valing = 'm'
-			t.border = False
-
-			for key, val in module_database.database.items():
-					t.add_row([key, val])
-
-			print (t)
-		elif args[0] == "options" and mm.moduleLoaded == 1:
-			moduleop.printoptions(modadd)
-		else:
-			raise UnknownCommand("module not loaded or unknown command")
-	except IndexError:
-		raise UnknownCommand("unknown command")
-
-def back(args):
-	if mm.moduleLoaded == 1:
-		mm.moduleLoaded = 0
-		mm.moduleName = ""
-	else:
-		raise UnknownCommand("unknown command")
-
-def reload(args):
-	if mm.moduleLoaded == 0:
-		try:
-			mod = "modules."+args[0]
-			imp.reload(sys.modules[mod])
-			sys.modules[mod]
-			print (colors.green+"module "+ args[0] +" reloaded"+colors.end)
-		except(IndexError):
-			print (colors.red+"please enter module's name"+colors.end)
-		except(KeyError):
-			print (colors.red+"module not found"+colors.end)
-	else:
-		try:
-			mod = "modules."+args[0]
-			imp.reload(sys.modules[mod])
-			sys.modules[mod]
-			print (colors.green+"module "+ args[0] +" reloaded"+colors.end)
-		except(IndexError):
-			mod = "modules."+mm.moduleName
-			imp.reload(sys.modules[mod])
-			sys.modules[mod]
-			print (colors.green+"module "+ mm.moduleName +" reloaded"+colors.end)
-		except(KeyError):
-			print (colors.red+"module not found"+colors.end)
-
-def run(args):
-	global modadd
-
-	if mm.moduleLoaded == 1:
-		try:
-			modadd.run()
-		except Exception as error:
-			print(colors.red+"error: module is corrupted\n")
-			traceback.print_exc(file=sys.stdout)
-			print(colors.end)
-	else:
-		raise UnknownCommand("module not loaded")
-
-def set(args):
-	global modadd
-	try:
-		targetvariable = modadd.variables[args[0]]
-		modadd.variables[args[0]] = args[1]
-		print(colors.green+args[0] +" => "+ args[1] + colors.end)
-
-	except(NameError, KeyError):
-		print(colors.red+"option not found"+colors.end)
-	except(IndexError):
-		print(colors.red+"please enter variable's value"+colors.end)
-
-def new(args):
-	try:
-		if args[0] == "module":
-			try:
-				completeName = os.path.join('modules', args[1]+".py")
-				if os.path.exists(completeName):
-					print(colors.red+"module already exists"+colors.end)
-
-				else:
-					mfile = open(completeName, 'w')
-					template = os.path.join('core', 'module_template')
-					f = open(template, 'r')
-					template_contents = f.readlines()
-					template_contents[6] = 'name = "'+args[1]+'"\n'
-					mfile.writelines(template_contents)
-					mfile.close()
-					print(colors.green+"module "+ args[1] +".py" +" created to modules folder"+colors.end)
-					print(colors.green+"done"+colors.end)
+				f = open('changelog', 'r')
+				file_contents = f.read()
+				print (colors.yellow+file_contents+colors.end)
+				f.close()
 			except(IOError):
-				print(colors.red+"something went wrong"+colors.end)
+				print(colors.red + "error: changelog file not found (have you removed it?)" + colors.end)
 
-			except(IndexError):
-				print(colors.red + "please enter module name"+ colors.end)
-		else:
-			raise UnknownCommand("unknown command")
-	except IndexError:
-		raise UnknownCommand("unkown command")
-
-def check(args):
-	try:
-		if args[0] == "module":
+	def use(self, args):
+		if self.mm.moduleLoaded == 0:
 			try:
-				modadd = globals()[args[1]]
-				print(colors.green+"module found"+colors.end)
-				check_module.modadd = modadd
-				check_module.check()
+				self.modadd = globals()[args[0]]
+				self.mm.moduleLoaded = 1
+				self.mm.setName(self.modadd.name)
+				try:
+					if self.modadd.outdated == 1:
+						print(colors.red+"this module is outdated and might not be working"+colors.end)
+				except (AttributeError):
+					pass
+				try:
+					if self.modadd.needroot == 1:
+						if not os.geteuid() == 0:
+							print(colors.red+"this module requires root permissions for full functionality!"+colors.end)
+				except(AttributeError):
+					pass
+				try:
+					self.modadd.init()
+				except(AttributeError):
+					pass
+			except KeyError:
+				print(colors.red + "module not found" + colors.end)
+			except IndexError:
+				print(colors.red + "please enter module name" + colors.end)
+		else:
+			raise UnknownCommand("module in use")
 
-			except(IndexError):
-				print(colors.red + "please enter module name"+ colors.end)
+	def show(self, args):
+		try:
+			if args[0] == "modules" and self.mm.moduleLoaded == 0:
+				t = PrettyTable([colors.green+'Modules:', ''+colors.end])
+				t.add_row(['',''])
+				t.align = 'l'
+				t.valing = 'm'
+				t.border = False
 
-			except Exception as error:
-				print(colors.red + "error: module not found ("+str(error)+")"+ colors.end)
+				for key, val in module_database.database.items():
+						t.add_row([key, val])
+
+				print (t)
+			elif args[0] == "options" and self.mm.moduleLoaded == 1:
+				moduleop.printoptions(self.modadd)
+			else:
+				raise UnknownCommand("module not loaded or unknown command")
+		except IndexError:
+			raise UnknownCommand("unknown command")
+
+	def back(self, args):
+		if self.mm.moduleLoaded == 1:
+			self.mm.moduleLoaded = 0
+			self.mm.moduleName = ""
 		else:
 			raise UnknownCommand("unknown command")
-	except IndexError:
-		raise UnknownCommand("unkown command")
 
-def matrix(args):
-	try:
-		core.matrix.main()
-	except KeyboardInterrupt:
-		curses.endwin()
-		curses.curs_set(1)
-		curses.reset_shell_mode()
-		curses.echo()
-
-def cowsay(args):
-	try:
-		message = ' '.join(args)
-		print(core.cowsay.cowsay(message))
-		return
-	except(ValueError):
-		print(core.cowsay.cowsay("µSploit Framework"))
-
-def ds(args):
-	print(dsag.darkside)
-
-def make(args):
-	try:
-		if args[0] == "exit":
-			sys.exit(0)
+	def reload(self, args):
+		if self.mm.moduleLoaded == 0:
+			try:
+				mod = "modules."+args[0]
+				imp.reload(sys.modules[mod])
+				sys.modules[mod]
+				print (colors.green+"module "+ args[0] +" reloaded"+colors.end)
+			except(IndexError):
+				print (colors.red+"please enter module's name"+colors.end)
+			except(KeyError):
+				print (colors.red+"module not found"+colors.end)
 		else:
-			raise UnknownCommand("unkown command")
-	except IndexError:
-		raise UnknownCommand("unkown command")
+			try:
+				mod = "modules."+args[0]
+				imp.reload(sys.modules[mod])
+				sys.modules[mod]
+				print (colors.green+"module "+ args[0] +" reloaded"+colors.end)
+			except(IndexError):
+				mod = "modules."+self.mm.moduleName
+				imp.reload(sys.modules[mod])
+				sys.modules[mod]
+				print (colors.green+"module "+ self.mm.moduleName +" reloaded"+colors.end)
+			except(KeyError):
+				print (colors.red+"module not found"+colors.end)
 
-def touchingsky(args):
-	core.touchingsky.main()
+	def run(self, args):
+
+		if self.mm.moduleLoaded == 1:
+			try:
+				self.modadd.run()
+			except Exception as error:
+				print(colors.red+"error: module is corrupted\n")
+				traceback.print_exc(file=sys.stdout)
+				print(colors.end)
+		else:
+			raise UnknownCommand("module not loaded")
+
+	def set(self, args):
+		try:
+			targetvariable = self.modadd.variables[args[0]]
+			self.modadd.variables[args[0]] = args[1]
+			print(colors.green+args[0] +" => "+ args[1] + colors.end)
+
+		except(NameError, KeyError):
+			print(colors.red+"option not found"+colors.end)
+		except(IndexError):
+			print(colors.red+"please enter variable's value"+colors.end)
+
+	def new(self, args):
+		try:
+			if args[0] == "module":
+				try:
+					completeName = os.path.join('modules', args[1]+".py")
+					if os.path.exists(completeName):
+						print(colors.red+"module already exists"+colors.end)
+
+					else:
+						mfile = open(completeName, 'w')
+						template = os.path.join('core', 'module_template')
+						f = open(template, 'r')
+						template_contents = f.readlines()
+						template_contents[6] = 'name = "'+args[1]+'"\n'
+						mfile.writelines(template_contents)
+						mfile.close()
+						print(colors.green+"module "+ args[1] +".py" +" created to modules folder"+colors.end)
+						print(colors.green+"done"+colors.end)
+				except(IOError):
+					print(colors.red+"something went wrong"+colors.end)
+
+				except(IndexError):
+					print(colors.red + "please enter module name"+ colors.end)
+			else:
+				raise UnknownCommand("unknown command")
+		except IndexError:
+			raise UnknownCommand("unkown command")
+
+	def check(self, args):
+		try:
+			if args[0] == "module":
+				try:
+					self.modadd = globals()[args[1]]
+					print(colors.green+"module found"+colors.end)
+					check_module.self.modadd = self.modadd
+					check_module.check()
+
+				except(IndexError):
+					print(colors.red + "please enter module name"+ colors.end)
+
+				except Exception as error:
+					print(colors.red + "error: module not found ("+str(error)+")"+ colors.end)
+			else:
+				raise UnknownCommand("unknown command")
+		except IndexError:
+			raise UnknownCommand("unkown command")
+
+	def matrix(self, args):
+		try:
+			core.matrix.main()
+		except KeyboardInterrupt:
+			curses.endwin()
+			curses.curs_set(1)
+			curses.reset_shell_mode()
+			curses.echo()
+
+	def cowsay(self, args):
+		try:
+			message = ' '.join(args)
+			print(core.cowsay.cowsay(message))
+			return
+		except(ValueError):
+			print(core.cowsay.cowsay("µSploit Framework"))
+
+	def ds(self, args):
+		print(dsag.darkside)
+
+	def make(self, args):
+		try:
+			if args[0] == "exit":
+				sys.exit(0)
+			else:
+				raise UnknownCommand("unkown command")
+		except IndexError:
+			raise UnknownCommand("unkown command")
+
+	def touchingsky(self, args):
+		core.touchingsky.main()
