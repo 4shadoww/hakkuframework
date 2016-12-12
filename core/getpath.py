57196a7f745c0230a35535ec39e09dbd240280e1
4shadoww <4shadoww0@gmail.com>
2016-12-12 17:32:52 +0200

    added new functions

--- core/getpath.py
+++ core/getpath.py
@@ -1,6 +1,22 @@
 #        Copyright (C) 2015 Noa-Emil Nissinen (4shadoww)
 import os
+import sys
 
+def main():
+	path = os.path.dirname(os.path.abspath(sys.modules['__main__'].__file__)) + "/"
+	return path
+
+def modules():
+	path = os.path.dirname(os.path.abspath(sys.modules['__main__'].__file__)) + "/modules/"
+	return path
+
+def core():
+	path = os.path.dirname(os.path.abspath(__file__)) + "/"
+	return path
+
+def libs():
+	path = os.path.dirname(os.path.abspath(__file__)) + "/libs/"
+	return path
 
 def tools():
 	path = os.path.dirname(os.path.abspath(__file__)) + "/tools/"
