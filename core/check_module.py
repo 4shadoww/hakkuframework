a1e20f896072a5d45f52ffd6fa71b660c642c729
4shadoww <4shadoww0@gmail.com>
2016-12-13 20:11:39 +0200

    fixed bug

--- core/check_module.py
+++ core/check_module.py
@@ -4,42 +4,50 @@ from core import colors
 import sys
 from glob import glob
 
+def testfailed():
+	print(colors.red+"\ntest not passed!\n"+colors.end)
+
 def check():
-	global modadd
+	try:
+		global modadd
 
-	print(colors.yellow+'\nchecking',modadd.name+colors.end)
+		print(colors.yellow+'\nchecking',modadd.conf["name"]+colors.end)
 
-	modadd.version
-	if modadd.desc == 'modules_description':
-		print(colors.red+'\ndesc variable has default value'+colors.end)
-		testfailed()
-	if modadd.github == 'mygithub':
-			print(colors.red+'\ngithub variable has default value'+colors.end)
+		modadd.conf["version"]
+		if modadd.conf["shortdesc"] == 'modules_description':
+			print(colors.red+'\ndesc variable has default value'+colors.end)
 			testfailed()
-	if modadd.createdby == 'creators_name':
-		print(colors.red+'\ncreatedby variable has default value'+colors.end)
-		testfailed()
-	if modadd.email == 'creators@email.com':
-		print(colors.red+'\nemail variable has default value'+colors.end)
-		testfailed()
-
-	for item in modadd.variables.items():
-		if item[0] == 'option1' or item[0] == 'option2' or item[1] == 'none1' or item[1] == 'none2':
-			print(colors.red+'\nvariables has default value')
+		if modadd.conf["github"] == 'mygithub':
+				print(colors.red+'\ngithub variable has default value'+colors.end)
+				testfailed()
+		if modadd.conf["author"] == 'creators_name':
+			print(colors.red+'\ncreatedby variable has default value'+colors.end)
+			testfailed()
+		if modadd.conf["email"] == 'creators@email.com':
+			print(colors.red+'\nemail variable has default value'+colors.end)
 			testfailed()
 
-	for desc in modadd.vdesc:
-		if desc == 'description1' or desc == 'description2':
-			print(colors.red+'\ndesc list has default value'+colors.end)
+		for item in modadd.variables.items():
+			if item[0] == 'option1' or item[0] == 'option2' or item[1] == 'none1' or item[1] == 'none2':
+				print(colors.red+'\nvariables has default value')
+				testfailed()
+
+		for desc in modadd.vdesc:
+			if desc == 'description1' or desc == 'description2':
+				print(colors.red+'\ndesc list has default value'+colors.end)
+				testfailed()
+		if len(modadd.variables) != len(modadd.vdesc):
+			print(colors.red+'\nvdesc has not same amount of items than variables')
 			testfailed()
-	if len(modadd.variables) != len(modadd.vdesc):
-		print(colors.red+'\nvdesc has not same amount of items than variables')
-		testfailed()
 
-	modadd.changelog
-	modadd.run
+		modadd.changelog
+		modadd.run
 
-	print(colors.green+'\ntest passed'+colors.end)
+		print(colors.green+'\ntest passed'+colors.end)
+	except:
+		print(colors.red+"error:\n")
+		traceback.print_exc()
+		print(colors.end)
 
 	try:
 		modadd.customcommands
