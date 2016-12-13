49f169203f6890aa0a6c8203a14fabfb2885ab2e
4shadoww <4shadoww0@gmail.com>
2016-12-13 20:13:12 +0200

    fix

--- core/info.py
+++ core/info.py
@@ -4,8 +4,8 @@ version = "2.0-alpha"
 update_date = "22.11.2016"
 codename = "Dark Side"
 
-about = (colors.blue+"version: "+colors.end+ '\xb5' +"Sploit Framework " + version
-+colors.blue+"\ncreated by:"+colors.end+" Noa-Emil Nissinen (4shadoww)"
-+colors.blue+"\nemail:"+colors.end+" 4shadoww0@gmail.com"
-+colors.blue+"\ngithub:"+colors.end+" 4shadoww"
-+colors.blue+"\ntwitter:"+colors.end+" dev4shadoww")
+about = ('\xb5' +"Sploit Framework "+version+
+"\nauthor: Noa-Emil Nissinen (4shadoww)"
+"\nemail: 4shadoww0@gmail.com"
+"\ngithub: 4shadoww"
+"\ntwitter: dev4shadoww")
