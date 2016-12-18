f2e7609274e008eca2d6770934837b6d9732b284
4shadoww <4shadoww0@gmail.com>
2016-12-18 17:12:07 +0200

    whois module addded

---
+++ modules/whois.py
@@ -0,0 +1,27 @@
+#        Copyright (C) 2015 Noa-Emil Nissinen (4shadoww)
+from core import colors
+from collections import OrderedDict
+import whois
+
+conf = {
+	"name": "whois", # Module's name (should be same as file name)
+	"version": "1.0", # Module version
+	"shortdesc": "perform whois query", # Short description
+	"github": "4shadoww", # Author's github
+	"author": "4shadoww", # Author
+	"email": "4shadoww0@gmail.com", # Email
+	"initdate": "18.12.2016", # Initial date
+}
+
+# List of the variables
+variables = OrderedDict((
+	("target", ["google.com", "target address"]),
+))
+
+# Simple changelog
+changelog = "Version 1.0:\nrelease"
+
+def run():
+	# Run
+	w = whois.whois(variables["target"][0])
+	print(w)
