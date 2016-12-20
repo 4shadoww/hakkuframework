17b7d98d123d37b1ce1a6992e6a9974c5a3eac17
4shadoww <4shadoww0@gmail.com>
2016-12-20 18:43:54 +0200

    update

--- core/info.py
+++ core/info.py
@@ -1,11 +1,11 @@
 from core import colors
 
-version = "2.0-alpha"
+version = "3.0-alpha"
+apiversion = "1.0-alpha"
 update_date = "22.11.2016"
 codename = "Dark Side"
 
-about = ('\xb5' +"Sploit Framework "+version+
+about = ("Hakku Framework "+version+
 "\nauthor: Noa-Emil Nissinen (4shadoww)"
 "\nemail: 4shadoww0@gmail.com"
-"\ngithub: 4shadoww"
-"\ntwitter: dev4shadoww")
+"\ngithub: 4shadoww")
