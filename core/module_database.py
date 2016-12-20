7f3fa1333d8589b25638c2e0110a931820ed8aa2
4shadoww <4shadoww0@gmail.com>
2016-12-20 16:51:16 +0200

    change

--- core/module_database.py
+++ core/module_database.py
@@ -5,35 +5,36 @@ from collections import OrderedDict
 
 
 database = OrderedDict((
-#wireless bluetooth
-(colors.uline+colors.blue+'Wireless / Bluetooth'+colors.end, colors.uline+colors.blue+'Description'+colors.end),
-('wifi_jammer', 'jam wifi'),
-('bluetooth_pod', 'bluetooth ping of death'),
-#end
-(' ',''),
-#web modules
-(colors.uline+colors.blue+'Web Modules'+colors.end, colors.uline+colors.blue+'Description'+colors.end),
-('cloudflare_resolver', 'tries to resolve real ip address from sub domains'),
-('dir_scanner', 'scan dirs from target address'),
-('web_killer', 'TCP Attack'),
-('apache_users', 'scan directory of apache users'),
-('pma_scanner', 'PHPMyAdmin login page scanner'),
-('port_scanner', 'scan open ports'),
-('email_bomber', 'spam emails'),
-('hostname_resolver', 'resolve hostname using ip'),
-('webserver_scout', 'get information from webserver'),
-('proxy_scout', 'scan http proxy from ip'),
-#end
-('  ',''),
-#network modules
-(colors.uline+colors.blue+'Network Modules'+colors.end, colors.uline+colors.blue+'Description'+colors.end),
-('network_kill', 'kicks out target device from network'),
-('arp_dos', 'arp cache denial of service attack'),
-('arp_spoof', 'arp spoof'),
-('mac_spoof', 'use fake mac address'),
-('mitm', 'man in the middle attack'),
-('dns_spoof', 'dns spoof'),
+# Wireless, bluetooth
+(colors.uline+colors.red+"Wireless / Bluetooth"+colors.end, colors.uline+colors.red+"Description"+colors.end),
+("wifi_jammer", "jam wifi"),
+("bluetooth_pod", "bluetooth ping of death"),
+# End of wireless, bluetooth modules
+(" ",""),
+# Web modules
+(colors.uline+colors.red+"Web Modules"+colors.end, colors.uline+colors.red+"Description"+colors.end),
+("cloudflare_resolver", "tries to resolve real ip address from sub domains"),
+("dir_scanner", "scan dirs from target address"),
+("web_killer", "TCP Attack"),
+("apache_users", "scan directory of apache users"),
+("pma_scanner", "PHPMyAdmin login page scanner"),
+("port_scanner", "scan open ports"),
+("email_bomber", "spam emails"),
+("hostname_resolver", "resolve hostname using ip"),
+("webserver_scout", "get information from webserver"),
+("proxy_scout", "scan http proxy from ip"),
+("whois", "perform whois query"),
+# End of web modules
+("  ",""),
+# Network modules
+(colors.uline+colors.red+"Network Modules"+colors.end, colors.uline+colors.red+"Description"+colors.end),
+("network_kill", "kicks out target device from network"),
+("arp_dos", "arp cache denial of service attack"),
+("arp_spoof", "arp spoof"),
+("mac_spoof", "use fake mac address"),
+("mitm", "man in the middle attack"),
+("dns_spoof", "dns spoof"),
 
-#end
-('   ',''),
+# End of network modules
+("   ",""),
 ))
