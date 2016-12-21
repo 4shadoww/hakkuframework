d897939ee214b6fa15075cfd2640dcc8e24ea593
4shadoww <4shadoww0@gmail.com>
2016-12-21 14:07:17 +0200

    update

--- core/helptable.py
+++ core/helptable.py
@@ -2,28 +2,28 @@ from core.prettytable import PrettyTable
 from core import colors
 
 def generateTable(hlist):
-	t = PrettyTable([colors.green+"Command"+colors.end, "",colors.green+"Description"+colors.end])
+	t = PrettyTable([colors.bold+"Command"+colors.end, "",colors.bold+"Description"+colors.end])
 	t.add_row(["-------"," ","-----------"])
 	t.align = 'l'
 	t.valing = 'm'
 	t.border = False
 
 	for key, val in hlist.items():
-			t.add_row([colors.yellow+key+colors.end, "  =>  ", val])
+			t.add_row([key, "  =>  ", val])
 
 	return t
 
 def generatemTable(hlist1, hlist2):
-	t = PrettyTable([colors.green+"Command"+colors.end, "",colors.green+"Description"+colors.end])
+	t = PrettyTable([colors.bold+"Command"+colors.end, "",colors.bold+"Description"+colors.end])
 	t.add_row(["-------"," ","-----------"])
 	t.align = 'l'
 	t.valing = 'm'
 	t.border = False
 
 	for key, val in hlist1.items():
-			t.add_row([colors.yellow+key+colors.end, "  =>  ", val])
+			t.add_row([key, "  =>  ", val])
 
 	for key, val in hlist2.items():
-			t.add_row([colors.yellow+key+colors.end, "  =>  ", val])
+			t.add_row([key, "  =>  ", val])
 
 	return t
