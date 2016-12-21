112aa3536c56d08362ac3fcd18aca382dec4ea7f
4shadoww <4shadoww0@gmail.com>
2016-12-21 14:06:56 +0200

    update

--- core/helpin.py
+++ core/helpin.py
@@ -3,24 +3,24 @@ from core import colors
 from collections import OrderedDict
 
 commands = OrderedDict((
-("use "+colors.end+"(module name)", "load module"),
+("use (module name)", "load module"),
 ("show modules", "show modules"),
 ("clear", "clear terminal"),
-("exit", "exit from "+ "\xb5" +"Sploit"),
-("version", "show "+ "\xb5" +"Sploit version"),
-("cl "+colors.end+"(command)","run terminal command"),
+("exit", "exit from Hakku Framework"),
+("version", "show Hakku Framework version"),
+("cl (command)","run terminal command"),
 ("ifconfig", "run terminal command ifconfig"),
 ("scan", "scan devices from lan"),
-("about", "information about "+ "\xb5" +"Sploit"),
+("about", "information about Hakku Framework"),
 ("changelog", "show changelog"),
-("new module "+colors.end+"(module name)", "create new module"),
-("check module "+colors.end+"(module name)", "check module"),
-("reload "+colors.end+"(module name)", "reload module (useful for developers)"),
+("new module (module name)", "create new module"),
+("check module (module name)", "check module"),
+("reload (module name)", "reload module (useful for developers)"),
 ))
 
 mcommands = OrderedDict((
 ("show options", "show options"),
-("set "+colors.end+"(option name) (value)", "set value to option"),
+("set (option name) (value)", "set value to option"),
 ("run", "run module"),
 ("exit", "exit from module"),
 ("back", "exit from module"),
@@ -28,7 +28,7 @@ mcommands = OrderedDict((
 ("ifconfig", "run terminal command ifconfig"),
 ("scan", "scan devices from lan"),
 ("about", "information about this module"),
-("cl "+colors.end+"(command)", "run terminal command"),
+("cl (command)", "run terminal command"),
 ("changelog", "show changelog"),
 ("reload", "reload this module (useful for developers)"),
 ))
