7e23ea1cd89b9fe98eb86ca53b8f55682176d6d1
4shadoww <4shadoww0@gmail.com>
2016-12-24 19:50:11 +0200

    fix

--- core/module_database.py
+++ core/module_database.py
@@ -1,47 +1,46 @@
-#        Copyright (C) 2015 Noa-Emil Nissinen (4shadoww)
+#        Copyright [C] 2015 Noa-Emil Nissinen [4shadoww]
 
 from core import colors
-from collections import OrderedDict
 
 
-database = OrderedDict((
+database = [
 # Wireless, bluetooth
-(colors.uline+colors.red+"Wireless / Bluetooth"+colors.end, colors.uline+colors.red+"Description"+colors.end),
-("wifi_jammer", "jam wifi"),
-("bluetooth_pod", "bluetooth ping of death"),
+[colors.uline+colors.red+"Wireless / Bluetooth"+colors.end, colors.uline+colors.red+"Description"+colors.end],
+["wifi_jammer", "jam wifi"],
+["bluetooth_pod", "bluetooth ping of death"],
 # End of wireless, bluetooth modules
-(" ",""),
+["",""],
 
 # Web modules
-(colors.uline+colors.red+"Web Modules"+colors.end, colors.uline+colors.red+"Description"+colors.end),
-("cloudflare_resolver", "tries to resolve real ip address from sub domains"),
-("dir_scanner", "scan dirs from target address"),
-("web_killer", "TCP Attack"),
-("apache_users", "scan directory of apache users"),
-("pma_scanner", "PHPMyAdmin login page scanner"),
-("port_scanner", "scan open ports"),
-("email_bomber", "spam emails"),
-("hostname_resolver", "resolve hostname using ip"),
-("webserver_scout", "get information from webserver"),
-("proxy_scout", "scan http proxy from ip"),
-("whois", "perform whois query"),
+[colors.uline+colors.red+"Web Modules"+colors.end, colors.uline+colors.red+"Description"+colors.end],
+["cloudflare_resolver", "tries to resolve real ip address from sub domains"],
+["dir_scanner", "scan dirs from target address"],
+["web_killer", "TCP Attack"],
+["apache_users", "scan directory of apache users"],
+["pma_scanner", "PHPMyAdmin login page scanner"],
+["port_scanner", "scan open ports"],
+["email_bomber", "spam emails"],
+["hostname_resolver", "resolve hostname using ip"],
+["webserver_scout", "get information from webserver"],
+["proxy_scout", "scan http proxy from ip"],
+["whois", "perform whois query"],
 # End of web modules
-(""," "),
+["",""],
 
 # Network modules
-(colors.uline+colors.red+"Network Modules"+colors.end, colors.uline+colors.red+"Description"+colors.end),
-("network_kill", "kicks out target device from network"),
-("arp_dos", "arp cache denial of service attack"),
-("arp_spoof", "arp spoof"),
-("mac_spoof", "use fake mac address"),
-("mitm", "man in the middle attack"),
-("dns_spoof", "dns spoof"),
+[colors.uline+colors.red+"Network Modules"+colors.end, colors.uline+colors.red+"Description"+colors.end],
+["network_kill", "kicks out target device from network"],
+["arp_dos", "arp cache denial of service attack"],
+["arp_spoof", "arp spoof"],
+["mac_spoof", "use fake mac address"],
+["mitm", "man in the middle attack"],
+["dns_spoof", "dns spoof"],
 # End of network modules
-("  ",""),
+["",""],
 
 # File modules
-(colors.uline+colors.red+"File modules"+colors.end, colors.uline+colors.red+"Description"+colors.end),
-("zip_cracker", "zip file brute-force attack using dictionary"),
+[colors.uline+colors.red+"File modules"+colors.end, colors.uline+colors.red+"Description"+colors.end],
+["zip_cracker", "zip file brute-force attack using dictionary"],
 
 # End of file modules
-))
+]
