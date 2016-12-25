ebb3cecbdc4db34c502ad59f5bebb3dfd51051ac
4shadoww <4shadoww0@gmail.com>
2016-12-25 14:38:52 +0200

    prettytable moved to lib directory

--- core/moduleop.py
+++ core/moduleop.py
@@ -3,7 +3,7 @@ import os
 from core import colors
 import traceback
 import sys
-from core.prettytable import PrettyTable
+from prettytable import PrettyTable
 from core import getpath
 
 def count():
