4d79bc679e68ea54e6544e93e1f7812fee7aa751
4shadoww <4shadoww0@gmail.com>
2016-12-27 11:26:51 +0200

    change

--- core/animline.py
+++ core/animline.py
@@ -1,5 +1,8 @@
 import sys
 
-def animline(text):
-	print(text, end="\r")
+def animline(text, last=False):
 	sys.stdout.write("\033[K")
+	if last == False:
+		sys.stdout.write(text+"\r")
+	else:
+		sys.stdout.write(text+"\n")
