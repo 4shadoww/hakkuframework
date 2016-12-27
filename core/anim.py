3d3dbf90d01e19d69a019ddd470008c3e96a1d2a
4shadoww <4shadoww0@gmail.com>
2016-12-27 20:36:43 +0200

    animline renamed to anim

---
+++ core/anim.py
@@ -0,0 +1,42 @@
+import sys
+from core import colors
+
+error = "["+colors.bold+colors.red+"err"+colors.end+"] "
+info = "["+colors.bold+colors.blue+"inf"+colors.end+"] "
+warning = "["+colors.bold+colors.yellow+"war"+colors.end+"] "
+success = "["+colors.bold+colors.green+"suf"+colors.end+"] "
+
+def animline(message, last=False):
+	sys.stdout.write("\033[K")
+	if last == False:
+		sys.stdout.write(message+"\r")
+	else:
+		sys.stdout.write(message+"\n")
+
+def animError(message, last=False)
+	sys.stdout.write("\033[K")
+	if last == False:
+		sys.stdout.write(error+message+"\r")
+	else:
+		sys.stdout.write(error+message+"\n")
+
+def animWarning(message, last=False)
+	sys.stdout.write("\033[K")
+	if last == False:
+		sys.stdout.write(warning+message+"\r")
+	else:
+		sys.stdout.write(warning+message+"\n")
+
+def animInfo(message, last=False)
+	sys.stdout.write("\033[K")
+	if last == False:
+		sys.stdout.write(info+message+"\r")
+	else:
+		sys.stdout.write(info+message+"\n")
+
+def animSuccess(message, last=False)
+	sys.stdout.write("\033[K")
+	if last == False:
+		sys.stdout.write(success+text+"\r")
+	else:
+		sys.stdout.write(success+text+"\n")
--- core/animline.py
+++
@@ -1,8 +0,0 @@
-import sys
-
-def animline(text, last=False):
-	sys.stdout.write("\033[K")
-	if last == False:
-		sys.stdout.write(text+"\r")
-	else:
-		sys.stdout.write(text+"\n")
