1119d9841b35fce5f8150ada9c90cefbf9a0fa12
4shadoww <4shadoww0@gmail.com>
2016-12-27 16:52:06 +0200

    added new function

--- core/moduleop.py
+++ core/moduleop.py
@@ -1,10 +1,13 @@
-#        Copyright (C) 2015 Noa-Emil Nissinen (4shadoww)
+#		Copyright (C) 2015 Noa-Emil Nissinen (4shadoww)
 import os
 from core import colors
 import traceback
 import sys
 from prettytable import PrettyTable
 from core import getpath
+from core import moddbparser
+from xml.etree import ElementTree
+from xml.dom import minidom
 
 def count():
 	isfile = os.path.isfile
@@ -38,3 +41,69 @@ def printoptions(modadd):
 		print(colors.red+"error: module is corrupted\n")
 		traceback.print_exc(file=sys.stdout)
 		print(colors.end)
+
+def writedb(root):
+	rough_string = ElementTree.tostring(root, 'utf-8').decode("utf-8").replace("\n", "").replace("\t", "").replace("  ", "").replace("    ", "").encode("utf-8")
+	reparsed = minidom.parseString(rough_string)
+	clean = reparsed.toprettyxml(indent="\t")
+	f = open(getpath.core()+"module_database.xml", "w")
+	f.write(clean)
+	f.close()
+
+def addtodb(modadd):
+	xml = moddbparser.parsemoddb()
+	root = xml[0]
+	tree = xml[1]
+
+	new = True
+	newcat = True
+
+	for category in root:
+		if category.tag == "category":
+			for item in category:
+				if item.tag == "module" and item.attrib["name"] == modadd.conf["name"]:
+					for info in item:
+						if info.tag == "shortdesc":
+							info.text = modadd.conf["shortdesc"]
+							new = False
+							tree.write(getpath.core()+"module_database.xml")
+							print("database updated")
+							return
+	if new == True:
+		print("\n"+modadd.conf["name"]+" doesn't exist in database")
+		print("available categorie keys:"+colors.yellow)
+		for category in root:
+			if category.tag == "category":
+				print(category.attrib["key"])
+		print(colors.end, end="")
+		catkey = input("\n\ngive new or exist key? ")
+
+		for category in root:
+			if category.tag == "category" and category.attrib["key"] == catkey:
+				module = ElementTree.Element("module")
+				shortdesc = ElementTree.Element("shortdesc")
+				shortdesc.text = modadd.conf["shortdesc"]
+				module.set("name", modadd.conf["name"])
+				module.append(shortdesc)
+				category.append(module)
+				writedb(root)
+				newcat = False
+				print("module added to "+category.attrib["name"])
+				break
+
+		if newcat == True:
+			print("category not found\ngoing to add new category")
+			catname = input("give new category's name: ")
+			newcat = ElementTree.Element("category")
+			newcat.set("name", catname)
+			newcat.set("key", catkey)
+			module = ElementTree.Element("module")
+			shortdesc = ElementTree.Element("shortdesc")
+			shortdesc.text = modadd.conf["shortdesc"]
+			module.set("name", modadd.conf["name"])
+			module.append(shortdesc)
+			newcat.append(module)
+			root.append(newcat)
+			writedb(root)
+			print("new category created")
+			print("module added to "+newcat.attrib["name"])
