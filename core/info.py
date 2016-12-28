4a31fd4654933d559e7a2b877960f7d651aab1c8
4shadoww <4shadoww0@gmail.com>
2016-12-28 15:31:43 +0200

    updated

--- core/info.py
+++ core/info.py
@@ -2,10 +2,10 @@ from core import colors
 
 version = "3.0-alpha"
 apiversion = "1.0-alpha"
-update_date = "22.11.2016"
-codename = "Dark Side"
+update_date = "28.12.2016"
+codename = "uranium"
 
-about = ("Hakku Framework "+version+
+about = ("Hakku Framework "+version+" "+codename+
 "\nauthor: Noa-Emil Nissinen (4shadoww)"
 "\nemail: 4shadoww0@gmail.com"
 "\ngithub: 4shadoww")
