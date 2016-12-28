6b11836faf747620810a9b73785a0bb980207f20
4shadoww <4shadoww0@gmail.com>
2016-12-28 15:32:07 +0200

    fix

--- core/network_scanner.py
+++ core/network_scanner.py
@@ -4,6 +4,7 @@ from scapy.all import srp,Ether,ARP,conf
 from datetime import datetime
 import traceback
 from core import colors
+from core.messages import *
 
 try:
 	import netifaces
@@ -14,32 +15,35 @@ except ImportError:
 
 
 def scan():
-	print(colors.blue+"interfaces:"+colors.end)
-	for iface in netifaces.interfaces():
-		print(colors.yellow+iface+colors.end)
-	print("")
-	interface = input(colors.purple+"interface: "+colors.end)
 	try:
-		ip = netifaces.ifaddresses(interface)[2][0]['addr']
-	except(ValueError, KeyError):
-		print(colors.red+"error: invalid interface"+colors.end)
-		return
-	ips = ip+"/24"
-	print(colors.green+"\n[*] scanning please wait...\n"+colors.end)
-	print(colors.blue+"MAC - IP"+colors.end)
+		print(colors.blue+"interfaces:"+colors.end)
+		for iface in netifaces.interfaces():
+			print(colors.yellow+iface+colors.end)
+		print("")
+		interface = input(colors.purple+"interface: "+colors.end)
+		try:
+			ip = netifaces.ifaddresses(interface)[2][0]['addr']
+		except(ValueError, KeyError):
+			printError("invalid interface")
+			return
+		ips = ip+"/24"
+		printInfo("scanning please wait...\n", start="\n")
+		print(colors.blue+"MAC - IP"+colors.end)
 
-	start_time = datetime.now()
+		start_time = datetime.now()
 
-	conf.verb = 0
-	try:
-		ans, unans = srp(Ether(dst="ff:ff:ff:ff:ff:ff")/ARP(pdst = ips), timeout = 2,iface=interface,inter=0.1)
-	except PermissionError:
-		print(colors.red+'error: root permissions required'+colors.end)
-		return
+		conf.verb = 0
+		try:
+			ans, unans = srp(Ether(dst="ff:ff:ff:ff:ff:ff")/ARP(pdst = ips), timeout = 2,iface=interface,inter=0.1)
+		except PermissionError:
+			printError('root permissions required')
+			return
 
-	for snd,rcv in ans:
-		print(rcv.sprintf(colors.yellow+"r%Ether.src% - %ARP.psrc%"+colors.end))
-	stop_time = datetime.now()
-	total_time = stop_time - start_time
-	print(colors.green+"\n[*] scan completed")
-	print("[*] scan duration:",total_time,colors.end)
+		for snd,rcv in ans:
+			print(rcv.sprintf(colors.yellow+"r%Ether.src% - %ARP.psrc%"+colors.end))
+		stop_time = datetime.now()
+		total_time = stop_time - start_time
+		printSuccess("scan completed", start="\n")
+		printSuccess("scan duration: "+str(total_time))
+	except KeyboardInterrupt:
+		printInfo("network scanner terminated", start="\n")
