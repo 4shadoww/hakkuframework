6e7bef12faa1e4632e362fe1294d8b8183d7cc65
4shadoww <4shadoww0@gmail.com>
2016-12-28 15:30:59 +0200

    fixed bug

--- core/messages.py
+++ core/messages.py
@@ -1,19 +1,19 @@
 import sys
 from core import colors
 
-error = "["+colors.bold+colors.red+"err"+colors.end+"] "
-info = "["+colors.bold+colors.blue+"inf"+colors.end+"] "
-warning = "["+colors.bold+colors.yellow+"war"+colors.end+"] "
-success = "["+colors.bold+colors.green+"suf"+colors.end+"] "
+errorstr = "["+colors.bold+colors.red+"err"+colors.end+"] "
+infostr = "["+colors.bold+colors.blue+"inf"+colors.end+"] "
+warningstr = "["+colors.bold+colors.yellow+"war"+colors.end+"] "
+successstr = "["+colors.bold+colors.green+"suf"+colors.end+"] "
 
-def printError(message, end="\n"):
-	sys.stdout.write(error+message+end)
+def printError(message, start="", end="\n"):
+	sys.stdout.write(errorstr+message+end)
 
-def printWarning(message, end="\n"):
-	sys.stdout.write(warning+message+end)
+def printWarning(message, start="", end="\n"):
+	sys.stdout.write(start+warningstr+message+end)
 
-def printInfo(message, end="\n"):
-	sys.stdout.write(info+message+end)
+def printInfo(message, start="", end="\n"):
+	sys.stdout.write(start+infostr+message+end)
 
-def printSuccess(message, end="\n"):
-	sys.stdout.write(success+message+end)
+def printSuccess(message, start="", end="\n"):
+	sys.stdout.write(start+successstr+message+end)
