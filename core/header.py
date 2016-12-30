2eff9ead6f7fcc40addaca82478cbe7c3620426f
4shadoww <4shadoww0@gmail.com>
2016-12-30 13:33:24 +0200

    updated

--- core/header.py
+++ core/header.py
@@ -15,7 +15,7 @@ def print_info():
 	count()
 
 	print("\t" + colors.bold + "Hakku Framework\n" + colors.end)
-	print("\t" + colors.bold + "Core "+colors.end+"\t[ "+info.version+" | "+info.codename+"\t]" + colors.end)
+	print("\t" + colors.bold + "Core "+colors.end+"\t[ "+info.version+" "+info.codename+"\t]" + colors.end)
 	print("\t" + colors.bold + "API"+colors.end+"\t[ "+info.apiversion+"\t\t]"+colors.end)
 	print("\t" + colors.bold + "Date"+colors.end+"\t[ "+info.update_date+"\t\t]"+colors.end)
 	print("\t" + colors.bold + "Modules "+colors.end+"[ "+count.mod+" modules"+"\t\t]"+colors.end)
