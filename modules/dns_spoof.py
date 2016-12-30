a66123a35d2e60d411ea070a35b7ff9f55e9abca
4shadoww <4shadoww0@gmail.com>
2016-12-30 11:19:16 +0200

    update

--- modules/dns_spoof.py
+++ modules/dns_spoof.py
@@ -27,7 +27,7 @@ conf = {
 	"github": "4shadoww",
 	"email": "4shadoww0@gmail.com",
 	"initdate": "29.4.2016",
-	"lastmod": "29.12.2016",
+	"lastmod": "30.12.2016",
 	"apisupport": False,
 	"needroot": 1,
 	"dependencies": ["libnetfilter-queue-dev", "python3.5-dev"]
