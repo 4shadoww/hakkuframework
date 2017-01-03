7cf922f0157d01a116b27574bb1eb440941765f0
4shadoww <4shadoww0@gmail.com>
2017-01-03 11:04:44 +0200

    updated

--- core/info.py
+++ core/info.py
@@ -2,7 +2,7 @@ from core import colors
 
 version = "3.1-alpha"
 apiversion = "1.0.1-alpha"
-update_date = "31.12.2016"
+update_date = "3.1.2017"
 codename = "uranium"
 
 about = ("Hakku Framework "+version+" "+codename+
