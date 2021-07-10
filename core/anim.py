6e664fd9871dff2846d3f39abf2cb5efe8c64a37
4shadoww <4shadoww0@gmail.com>
2021-07-11 00:26:33 +0300

    change indent, fix

--- core/anim.py
+++ core/anim.py
@@ -1,4 +1,4 @@
-#		Copyright (C) 2015 Noa-Emil Nissinen (4shadoww)
+#        Copyright (C) 2015 Noa-Emil Nissinen (4shadoww)
 
 import sys
 from core import colors
@@ -9,36 +9,36 @@ warningstr = "["+colors.bold+colors.yellow+"war"+colors.end+"] "
 successstr = "["+colors.bold+colors.green+"suf"+colors.end+"] "
 
 def animline(message, last=False):
-	sys.stdout.write("\033[K")
-	if last == False:
-		sys.stdout.write(message+"\r")
-	else:
-		sys.stdout.write(message+"\n")
+    sys.stdout.write("\033[K")
+    if last == False:
+        sys.stdout.write(message+"\r")
+    else:
+        sys.stdout.write(message+"\n")
 
 def animError(message, last=False):
-	sys.stdout.write("\033[K")
-	if last == False:
-		sys.stdout.write(errorstr+message+"\r")
-	else:
-		sys.stdout.write(errorstr+message+"\n")
+    sys.stdout.write("\033[K")
+    if last == False:
+        sys.stdout.write(errorstr+message+"\r")
+    else:
+        sys.stdout.write(errorstr+message+"\n")
 
 def animWarning(message, last=False):
-	sys.stdout.write("\033[K")
-	if last == False:
-		sys.stdout.write(warningstr+message+"\r")
-	else:
-		sys.stdout.write(warningstr+message+"\n")
+    sys.stdout.write("\033[K")
+    if last == False:
+        sys.stdout.write(warningstr+message+"\r")
+    else:
+        sys.stdout.write(warningstr+message+"\n")
 
 def animInfo(message, last=False):
-	sys.stdout.write("\033[K")
-	if last == False:
-		sys.stdout.write(infostr+message+"\r")
-	else:
-		sys.stdout.write(infostr+message+"\n")
+    sys.stdout.write("\033[K")
+    if last == False:
+        sys.stdout.write(infostr+message+"\r")
+    else:
+        sys.stdout.write(infostr+message+"\n")
 
 def animSuccess(message, last=False):
-	sys.stdout.write("\033[K")
-	if last == False:
-		sys.stdout.write(successstr+text+"\r")
-	else:
-		sys.stdout.write(successstr+text+"\n")
+    sys.stdout.write("\033[K")
+    if last == False:
+        sys.stdout.write(successstr+message+"\r")
+    else:
+        sys.stdout.write(successstr+message+"\n")
