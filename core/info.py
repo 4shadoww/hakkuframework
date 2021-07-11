77a7065e8ed6e06f91bb3b625c5a95cc6bce6a03
4shadoww <4shadoww0@gmail.com>
2021-07-11 04:49:19 +0300

    update

--- core/info.py
+++ core/info.py
@@ -1,9 +1,9 @@
 from core import colors
 
-version = "3.1-alpha"
-apiversion = "1.0.1-alpha"
-update_date = "3.1.2017"
-codename = "uranium"
+version = "pre-1.0"
+apiversion = "pre-1.0"
+update_date = "xxxx-xx-xx"
+codename = "phoenix"
 
 about = ("Hakku Framework "+version+" "+codename+
 "\nauthor: Noa-Emil Nissinen (4shadoww)"
